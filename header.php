ca2aeb0e42b731561d97c91711117c2f06fb812b
antoni-devlin <antoni.devlin@gmail.com>
2019-05-09 14:27:57 +0100

    Minor changes

--- header.php
+++ header.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+  	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133685561-1"></script>
+  	<script>
+    		window.dataLayer = window.dataLayer || [];
+    		function gtag(){dataLayer.push(arguments);}
+    		gtag('js', new Date());
+    		gtag('config', 'UA-133685561-1');
+  	</script>
+
+  	<meta name="google-site-verification" content="C9Fif26YTwP4YXyeEvBxExSdgMK8B8owZOZ1vjdoKaU" />
+
+          <meta charset="utf-8">
+          <title>DVLN</title>
+
     <?php wp_head(); ?>
   </head>
   <body>
--- single.php
+++ single.php
@@ -1,12 +1,34 @@
+<?php get_header(); ?>
+
+<style>
+body {
+  background-color: white;
+}
+</style>
+
 <?php
 
   while(have_posts()) {
     the_post(); ?>
-    <h1>This is a page, not a post.</h1>
-    <h2><?php the_title(); ?></h2>
-    <p> <?php the_content(); ?> </p>
+
+    <article class="post post_detail">
+        <div class="post_image_heading">
+            <img alt='test' src=" <?php the_post_thumbnail();?> " >
+            <div class="image-text">
+                <h3><?php the_title(); ?></h3>
+            </div>
+        </div>
+        <p><?php the_content(); ?></p> 
+        <div class="tags">
+            <p>
+                    <a href="#">#TestCategory</a>
+            </p>
+        </div>
+    </article>
 
   <?php }
 
 
  ?>
+
+ <?php get_footer();?>
