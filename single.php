a11ce7ea6d51e25a85a1c339c4b8b420fa16a055
antoni-devlin <antoni.devlin@gmail.com>
2019-05-10 13:13:55 +0100

    Fixed single.php

--- single.php
+++ single.php
@@ -1,34 +1,59 @@
 <?php get_header(); ?>
+    <body>
+      <nav>
+          <ul>
+              <?php if (has_site_icon()) { ?>
+                <li><a href="/"><img class="logo" src="/media/dvln-logo.png" alt=""></a></li>
+              <?php } else { ?>
+                <li><a href="/">Home</a></li>
+              <?php } ?>
 
-<style>
-body {
-  background-color: white;
-}
-</style>
 
-<?php
+              <li><a href="#">About</a></li>
+              <li><a href="#">CV</a></li>
+          </ul>
+      </nav>
+        <div class="container">
+            <aside class="sidebar_left">
+            </aside>
+            <main class="main_col">
 
-  while(have_posts()) {
-    the_post(); ?>
+	<style>
+  	body {
+  		background-color: white;
+  	}
+	</style>
+            <?php
+            while(have_posts()) {
+              the_post(); ?>
+            <article class="post post_detail">
+                <div class="post_image_heading">
+                    <img src=" <?php the_post_thumbnail_url();?> " alt="chris-arock-199799-unsplash_J7R3sga.jpg">
+                    <div class="image-text">
+                        <h3> <?php the_title(); ?> </h3>
+                    </div>
+                </div>
+                <p>  <?php the_content(); ?> </p>
+                <!-- Feb. 17, 2019, 10:29 a.m. -->
+                <div class="tags">
+                    <p>
 
-    <article class="post post_detail">
-        <div class="post_image_heading">
-            <img src=<?php the_post_thumbnail_url();?> alt="">
-            <div class="image-text">
-                <h3> <?php the_title(); ?></h3>
-            </div>
+
+                            <a href="#">#Python</a>
+
+                            <a href="#">#Stories</a>
+
+
+                    </p>
+                </div>
+            </article>
+          <?php }
+
+
+         ?>
+
+            </main>
+            <aside class="sidebar_right">
+            </aside>
         </div>
-        <p><?php the_content(); ?></p>
-        <div class="tags">
-            <p>
-                    <a href="#">#TestCategory</a>
-            </p>
-        </div>
-    </article>
-
-  <?php }
-
-
- ?>
-
- <?php get_footer();?>
+<?php get_footer(); ?>
