5735270d1485388afa1105b3362caa74d9f697fd
antoni-devlin <antoni.devlin@gmail.com>
2019-05-12 15:22:30 +0100

    Created basic page template

--- page.php
+++ page.php
@@ -1,11 +1,35 @@
+<?php get_header(); ?>
+
 <?php
 
-  while(have_posts()) {
-    the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <p> <?php the_content(); ?> </p>
-
-  <?php }
-
+$custom_logo_id = get_theme_mod( 'custom_logo' );
+$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 
  ?>
+
+    <body>
+        <nav>
+            <ul>
+                <?php if (has_custom_logo()): ?>
+                <li><a href="/"><img class="logo" src=" <?php echo $image[0]; ?> " alt=""></a></li>
+                <?php else: ?>
+                <li> <a href="/">Home</a></li>
+                <?php endif; ?>
+                <li><a href="#">About</a></li>
+                <li><a href="#">CV</a></li>
+            </ul>
+        </nav>
+        <div class="container">
+            <aside class="sidebar_left">
+            </aside>
+            <main class="main_col">
+                <h1> <?php the_title(); ?> </h1>
+                <p> <?php the_content(); ?> </p>
+
+
+
+            </main>
+            <aside class="sidebar_right">
+            </aside>
+        </div>
+      <?php get_footer(); ?>
